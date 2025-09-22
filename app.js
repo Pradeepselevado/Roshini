6315cc5e0901cd4a751fdb71ff82a102cfac168a
Pradeepselevado <spradeep@elevadosoftwares.com>
2025-09-22 15:13:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const routers = express.Router();
 // Use separate route files
 
 // routers.use('/api', require("./routes/Categoryroute"));
-routers.use('/newsfeed', require("./routes/Newsfeedroute"));
-routers.use('/user', require("./routes/Userroute"));
+routers.use('/newsfeed', require("./Routes/Newsfeedroute"));
+routers.use('/user', require("./Routes/Userroute"));
 
 module.exports = routers;
